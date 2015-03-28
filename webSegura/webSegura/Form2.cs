3ff3b26dbefc7e89196dd9b25cedae3f813188a4
areyes <areyes@bec.seguridad.unam.mx>
2015-03-28 00:36:15 -0600

    aplicacion cliente

---
+++ webSegura/webSegura/Form2.cs
@@ -0,0 +1,47 @@
+﻿//Autor: Esau Reyes Valdespino
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TweetSharp;
+
+namespace webSegura
+{
+    public partial class Form2 : Form
+    {
+        
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {            
+            
+            TwitterService service = new TwitterService("CyF53YntotaWYggs5cmk3rHHv", "DtQAwLu6WH17YlyInuHUwjUtwbWqzZaNzffiKz7Lkq7k2APGgq");
+            OAuthRequestToken requestToken = service.GetRequestToken();
+            Uri uri = service.GetAuthorizationUri(requestToken);
+            webBrowser1.Navigate(uri);
+            string verifier = "12345";
+            OAuthAccessToken access = service.GetAccessToken(requestToken,verifier);            
+            service.AuthenticateWith(access.Token,access.TokenSecret);
+                       
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+        }
+
+    }
+
+    
+
+
+}
---
+++ webSegura/webSegura/loginFT.cs
@@ -0,0 +1,38 @@
+﻿//Autor: Esau Reyes Valdespino
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace webSegura
+{
+    public partial class loginFT : Form
+    {
+        public loginFT()
+        {
+            InitializeComponent();
+        }
+
+        private void loginFT_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 logF = new Form1();
+            logF.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 logT = new Form2();
+            logT.Show();
+        }
+    }
+}
